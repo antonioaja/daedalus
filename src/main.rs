8a4bd1e29d81f14ee928ab31be3c903da6f3ae37
Antonio <antonioaja@protonmail.com>
2023-04-30 10:18:27 -0400

    Added better errors

--- src/main.rs
+++ src/main.rs
@@ -53,19 +53,23 @@ fn main() -> Result<()> {
         // Calculate read/write operations
         let mut buffer: [u8; 1000000] = [0; 1000000];
         let mut leftovers: Vec<u8> = vec![];
-        let file_bytes = file.metadata().unwrap().len();
+        let file_bytes = file
+            .metadata()
+            .context(format!("Could not get size of {}", &args.file))?
+            .len();
         let full_chunks = file_bytes / buffer.len() as u64;
         let leftover_bytes = file_bytes % buffer.len() as u64;
 
         // Hash the file
         let mut hasher = blake3::Hasher::new();
         for _ in 0..full_chunks {
-            file.read_exact(&mut buffer).expect("Could not fill buffer");
+            file.read_exact(&mut buffer)
+                .context("Could not fill buffer for file hashing before encryption!")?;
             hasher.update(&buffer);
         }
         if leftover_bytes != 0 {
             file.read_to_end(&mut leftovers)
-                .expect("Could not fill buffer");
+                .context("Could not fill buffer for file hashing before encryption!")?;
             hasher.update(&leftovers);
         }
         let hash: [u8; 32] = hasher.finalize().as_bytes().clone();
@@ -74,48 +78,50 @@ fn main() -> Result<()> {
         let iv = rand::random::<[u8; 16]>();
 
         // Create the output file
-        let mut enc_file = File::create(args.file.clone() + ".daedalus").context(format!(
-            "Could not create {}",
-            args.file.clone() + ".daedalus"
-        ))?;
+        let output_file_name: &str = &(args.file.clone() + ".daedalus");
+        let mut enc_file = File::create(output_file_name)
+            .context(format!("Could not create {}", output_file_name))?;
 
         // Insert header, file hash, & IV
         enc_file
             .write_all(HEADER_AES256_ARGON_FIB)
-            .expect("Could not write header to output!");
+            .context("Could not write header to encrypted output!")?;
         enc_file
             .write_all(&hash)
-            .expect("Could not write file hash to output!");
+            .context("Could not write file hash to encrypted output!")?;
         enc_file
             .write_all(&iv)
-            .expect("Could not write IV to output!");
+            .context("Could not write IV to encrypted output!")?;
 
         // Encrypt the file
-        file.rewind().expect("Could not return to start of file!");
+        file.rewind()
+            .context(format!("Could not return to start of {}!", &args.file))?;
         let mut buffer: [u8; 1000000] = [0; 1000000];
         let mut leftovers: Vec<u8> = vec![];
         for _ in 0..full_chunks {
-            file.read_exact(&mut buffer).expect("Could not fill buffer");
+            file.read_exact(&mut buffer)
+                .context("Could not fill buffer for file encryption!")?;
             let encrypted = cipher.cbc_encrypt(&iv, &buffer);
-            enc_file.write_all(&encrypted).context(format!(
-                "Could not write to {}",
-                args.file.clone() + ".daedalus"
-            ))?;
+            enc_file
+                .write_all(&encrypted)
+                .context(format!("Could not write to {}", output_file_name))?;
         }
         if leftover_bytes != 0 {
             file.read_to_end(&mut leftovers)
-                .expect("Could not fill buffer");
+                .context("Could not fill buffer for file encryption!")?;
             let encrypted = cipher.cbc_encrypt(&iv, &leftovers);
-            enc_file.write_all(&encrypted).context(format!(
-                "Could not write to {}",
-                args.file.clone() + ".daedalus"
-            ))?;
+            enc_file
+                .write_all(&encrypted)
+                .context(format!("Could not write to {}", output_file_name))?;
         }
     } else if extension(&args.file) == ".daedalus" {
         // Calculate read/write operations
         let mut buffer: [u8; 1000016] = [0; 1000016];
         let mut leftovers: Vec<u8> = vec![];
-        let file_bytes = file.metadata().unwrap().len();
+        let file_bytes = file
+            .metadata()
+            .context(format!("Could not get size of {}", &args.file))?
+            .len();
         let full_chunks = (file_bytes - 80) / buffer.len() as u64;
         let leftover_bytes = (file_bytes - 80) % buffer.len() as u64;
 
@@ -124,51 +130,50 @@ fn main() -> Result<()> {
         let mut input_hash: [u8; 32] = [0; 32];
         let mut iv: [u8; 16] = [0; 16];
         file.read_exact(&mut header)
-            .expect("Could not read header!");
+            .context("Could not read header from encrypted input!")?;
         file.read_exact(&mut input_hash)
-            .expect("Could not read hash!");
-        file.read_exact(&mut iv).expect("Could not read IV!");
+            .context("Could not read hash from encrypted input!")?;
+        file.read_exact(&mut iv)
+            .context("Could not read IV from encrypted input!")?;
 
         // Create the output file
-        let mut dec_file = File::create(&args.file.replace(".daedalus", "")).context(format!(
-            "Could not create {}",
-            &args.file.replace(".daedalus", "")
-        ))?;
+        let output_file_name = &args.file.replace(".daedalus", "");
+        let mut dec_file = File::create(output_file_name)
+            .context(format!("Could not create {}", output_file_name))?;
 
         // Decrypt the file
         for _ in 0..full_chunks {
-            file.read_exact(&mut buffer).expect("Could not fill buffer");
+            file.read_exact(&mut buffer)
+                .context("Could not fill buffer for file decryption!")?;
             let decrypted = cipher.cbc_decrypt(&iv, &buffer);
-            dec_file.write_all(&decrypted).context(format!(
-                "Could not write to {}",
-                args.file.clone() + ".daedalus"
-            ))?;
+            dec_file
+                .write_all(&decrypted)
+                .context(format!("Could not write to {}", output_file_name))?;
         }
         if leftover_bytes != 0 {
             file.read_to_end(&mut leftovers)
-                .expect("Could not fill buffer");
+                .context("Could not fill buffer for file decryption!")?;
             let decrypted = cipher.cbc_decrypt(&iv, &leftovers);
-            dec_file.write_all(&decrypted).context(format!(
-                "Could not write to {}",
-                args.file.clone() + ".daedalus"
-            ))?;
+            dec_file
+                .write_all(&decrypted)
+                .context(format!("Could not write to {}", output_file_name))?;
         }
 
         // Calculate and compare hashes
-        let mut dec_file = File::open(&args.file.replace(".daedalus", "")).unwrap();
+        let mut dec_file = File::open(output_file_name).unwrap();
         let mut buffer: [u8; 1000016] = [0; 1000016];
         let mut leftovers: Vec<u8> = vec![];
         let mut hasher = blake3::Hasher::new();
         for _ in 0..full_chunks {
             dec_file
                 .read_exact(&mut buffer)
-                .expect("Could not fill buffer");
+                .context("Could not fill buffer for file hashing after decryption!")?;
             hasher.update(&buffer);
         }
         if leftover_bytes != 0 {
             dec_file
                 .read_to_end(&mut leftovers)
-                .expect("Could not fill buffer");
+                .context("Could not fill buffer for file hashing after decryption!")?;
             hasher.update(&leftovers);
         }
         if &input_hash != hasher.finalize().as_bytes() {
@@ -178,7 +183,9 @@ fn main() -> Result<()> {
         println!("Something went wrong! Invalid extension.\nThis should be impossible. Contact developer.");
     }
 
-    println!("\nTime Elapsed: {} ms", now.elapsed().as_millis());
+    if args.verbose {
+        println!("Time Elapsed: {} ms", now.elapsed().as_millis());
+    }
 
     Ok(())
 }

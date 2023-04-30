c25fb5e01ad9c6db98c282aa879f844f08507133
Antonio <antonioaja@protonmail.com>
2023-04-29 23:24:19 -0400

    Full code revamp for any size file

--- src/main.rs
+++ src/main.rs
@@ -4,6 +4,7 @@ use clap::Parser;
 use libaes::Cipher;
 use std::fs::File;
 use std::io::prelude::*;
+use std::time::*;
 
 #[derive(Parser, Debug)]
 #[clap(
@@ -20,16 +21,17 @@ struct Args {
     #[clap(short, long, value_parser)]
     password: String,
 
-    /// Encrypts the input
+    /// Activates verbose output
     #[clap(short, long, value_parser, default_value_t = false)]
-    encrypt: bool,
-
-    /// Decrypts the input
-    #[clap(short, long, value_parser, default_value_t = false)]
-    decrypt: bool,
+    verbose: bool,
 }
 
+const HEADER_AES256_ARGON_FIB: &[u8; 32] = b"DAEDALUSAES256ARGON2FIBONAglowie";
+
 fn main() -> Result<()> {
+    // Start timer
+    let now: Instant = Instant::now();
+
     // Gather input arguments
     let args: Args = Args::parse();
 
@@ -46,49 +48,137 @@ fn main() -> Result<()> {
 
     // Read input file into memory
     let mut file = File::open(&args.file).context(format!("Could not open {}", &args.file))?;
-    let mut contents: Vec<u8> = vec![];
-    file.read_to_end(&mut contents)
-        .context(format!("Could not read {}", &args.file))?;
 
-    if args.encrypt && !args.decrypt {
-        // Prepend file with IV data (random 16 bytes)
+    if extension(&args.file) != ".daedalus" {
+        // Calculate read/write operations
+        let mut buffer: [u8; 1000000] = [0; 1000000];
+        let mut leftovers: Vec<u8> = vec![];
+        let file_bytes = file.metadata().unwrap().len();
+        let full_chunks = file_bytes / buffer.len() as u64;
+        let leftover_bytes = file_bytes % buffer.len() as u64;
+
+        // Hash the file
+        let mut hasher = blake3::Hasher::new();
+        for _ in 0..full_chunks {
+            file.read_exact(&mut buffer).expect("Could not fill buffer");
+            hasher.update(&buffer);
+        }
+        if leftover_bytes != 0 {
+            file.read_to_end(&mut leftovers)
+                .expect("Could not fill buffer");
+            hasher.update(&leftovers);
+        }
+        let hash: [u8; 32] = hasher.finalize().as_bytes().clone();
+
+        // Calculate random IV
         let iv = rand::random::<[u8; 16]>();
 
-        for i in 0..16 {
-            contents.insert(i, iv[i]);
-        }
-
-        // Encrypt the input file
-        let encrypted = cipher.cbc_encrypt(&iv, &contents);
+        // Create the output file
         let mut enc_file = File::create(args.file.clone() + ".daedalus").context(format!(
             "Could not create {}",
             args.file.clone() + ".daedalus"
         ))?;
-        enc_file.write_all(&encrypted).context(format!(
-            "Could not write to {}",
-            args.file.clone() + ".daedalus"
-        ))?;
-    } else if args.decrypt && !args.encrypt {
-        // Decrypt the input file
-        let mut decrypted = cipher.cbc_decrypt(&contents[0..16], &contents);
 
-        // Remove IV data
-        for _ in 0..16 {
-            decrypted.remove(0);
+        // Insert header, file hash, & IV
+        enc_file
+            .write_all(HEADER_AES256_ARGON_FIB)
+            .expect("Could not write header to output!");
+        enc_file
+            .write_all(&hash)
+            .expect("Could not write file hash to output!");
+        enc_file
+            .write_all(&iv)
+            .expect("Could not write IV to output!");
+
+        // Encrypt the file
+        file.rewind().expect("Could not return to start of file!");
+        let mut buffer: [u8; 1000000] = [0; 1000000];
+        let mut leftovers: Vec<u8> = vec![];
+        for _ in 0..full_chunks {
+            file.read_exact(&mut buffer).expect("Could not fill buffer");
+            let encrypted = cipher.cbc_encrypt(&iv, &buffer);
+            enc_file.write_all(&encrypted).context(format!(
+                "Could not write to {}",
+                args.file.clone() + ".daedalus"
+            ))?;
         }
+        if leftover_bytes != 0 {
+            file.read_to_end(&mut leftovers)
+                .expect("Could not fill buffer");
+            let encrypted = cipher.cbc_encrypt(&iv, &leftovers);
+            enc_file.write_all(&encrypted).context(format!(
+                "Could not write to {}",
+                args.file.clone() + ".daedalus"
+            ))?;
+        }
+    } else if extension(&args.file) == ".daedalus" {
+        // Calculate read/write operations
+        let mut buffer: [u8; 1000016] = [0; 1000016];
+        let mut leftovers: Vec<u8> = vec![];
+        let file_bytes = file.metadata().unwrap().len();
+        let full_chunks = (file_bytes - 80) / buffer.len() as u64;
+        let leftover_bytes = (file_bytes - 80) % buffer.len() as u64;
 
-        // Write out decrypted file
+        // Read header information
+        let mut header: [u8; 32] = [0; 32];
+        let mut input_hash: [u8; 32] = [0; 32];
+        let mut iv: [u8; 16] = [0; 16];
+        file.read_exact(&mut header)
+            .expect("Could not read header!");
+        file.read_exact(&mut input_hash)
+            .expect("Could not read hash!");
+        file.read_exact(&mut iv).expect("Could not read IV!");
+
+        // Create the output file
         let mut dec_file = File::create(&args.file.replace(".daedalus", "")).context(format!(
             "Could not create {}",
             &args.file.replace(".daedalus", "")
         ))?;
-        dec_file.write_all(&decrypted).context(format!(
-            "Could not write to {}",
-            "dec_".to_owned() + &args.file.clone()
-        ))?;
+
+        // Decrypt the file
+        for _ in 0..full_chunks {
+            file.read_exact(&mut buffer).expect("Could not fill buffer");
+            let decrypted = cipher.cbc_decrypt(&iv, &buffer);
+            dec_file.write_all(&decrypted).context(format!(
+                "Could not write to {}",
+                args.file.clone() + ".daedalus"
+            ))?;
+        }
+        if leftover_bytes != 0 {
+            file.read_to_end(&mut leftovers)
+                .expect("Could not fill buffer");
+            let decrypted = cipher.cbc_decrypt(&iv, &leftovers);
+            dec_file.write_all(&decrypted).context(format!(
+                "Could not write to {}",
+                args.file.clone() + ".daedalus"
+            ))?;
+        }
+
+        // Calculate and compare hashes
+        let mut dec_file = File::open(&args.file.replace(".daedalus", "")).unwrap();
+        let mut buffer: [u8; 1000016] = [0; 1000016];
+        let mut leftovers: Vec<u8> = vec![];
+        let mut hasher = blake3::Hasher::new();
+        for _ in 0..full_chunks {
+            dec_file
+                .read_exact(&mut buffer)
+                .expect("Could not fill buffer");
+            hasher.update(&buffer);
+        }
+        if leftover_bytes != 0 {
+            dec_file
+                .read_to_end(&mut leftovers)
+                .expect("Could not fill buffer");
+            hasher.update(&leftovers);
+        }
+        if &input_hash != hasher.finalize().as_bytes() {
+            println!("Given hash of encrypted file does not match computed hash.\nCorruption or tampering may have occurred.")
+        }
     } else {
-        println!("The option to decrypt or encrypt wasn't selected, or both were selected.\nPlease pick one!");
+        println!("Something went wrong! Invalid extension.\nThis should be impossible. Contact developer.");
     }
+
+    println!("\nTime Elapsed: {} ms", now.elapsed().as_millis());
 
     Ok(())
 }
@@ -120,4 +210,12 @@ pub fn fibonacci_salter(pwd_len: usize) -> String {
     }
 
     return out_salt;
+}
+
+pub fn extension(filename: &str) -> &str {
+    filename
+        .rfind('.')
+        .map(|idx| &filename[idx..])
+        .filter(|ext| ext.chars().skip(1).all(|c| c.is_ascii_alphanumeric()))
+        .unwrap_or("")
 }
